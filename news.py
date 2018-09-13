a2e962e43d9e045bdb10f2583a766bbf5c99d263
akarshsingh9 <akarsh.singh9@gmail.com>
2018-09-13 22:01:27 +0530

    news.py initial updated

--- news.py
+++ news.py
@@ -5,3 +5,22 @@ import sys
 apiKey = '59f3471779b0400b8d99de8a298673e2'
 newsURL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey='+apiKey
 
+def news():
+    try:
+        r = requests.get(newsURL)
+        d = r.json()
+        if d['status'] != 'error':
+            #print(d)
+            # aritcles is the json array, 0,1,2 obj indices - different articles, then access other dict elements
+            for i in range(int(d['totalResults']/5)):
+                print('Source: ',d['articles'][i]['source']['name'])
+                print('Title: ',d['articles'][i]['title'])
+                print('Description: ',d['articles'][i]['description'])
+        else:
+            print(d['code'])
+            print(d['message'])
+    except:
+        print("Exception found")
+
+
+news()
